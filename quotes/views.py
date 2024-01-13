3122cb60d23340194f23970a223f22c6c0198ee4
Prakhar-Shankar <prakharshankar247@gmail.com>
2024-01-14 02:35:48 +0530

    trying to fetch data from api but there is some error

--- quotes/views.py
+++ quotes/views.py
@@ -3,7 +3,17 @@ from django.shortcuts import render
 #pk_1d29f40ff033462a8b4783bfae48aa78
 
 def home(request):
-    return render(request, 'home.html', {})
+    import requests
+    import json
+
+    api_request = requests.get("https://cloud.iexapis.com/stable/stock/aapl/quote?token=pk_1d29f40ff033462a8b4783bfae48aa78")
+
+    try:
+        api = json.loads(api_request.content)
+    except Exception as e:
+        api = "Error..."
+
+    return render(request, 'home.html', {'api': api})
     
 def about(request):
     return render(request, 'about.html', {})
