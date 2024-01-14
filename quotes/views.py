fcfb8ad05aba3c2105c6c80a1f97edc608642c7d
Prakhar-Shankar <prakharshankar247@gmail.com>
2024-01-14 18:17:17 +0530

    Search button is fetching the correct data from the API and giving us the correct results

--- quotes/views.py
+++ quotes/views.py
@@ -6,14 +6,26 @@ def home(request):
     import requests
     import json
 
-    api_request = requests.get("https://cloud.iexapis.com/stable/stock/aapl/quote?token=pk_1d29f40ff033462a8b4783bfae48aa78")
+    if request.method == "POST":
+        ticker = request.POST['ticker']
 
-    try:
-        api = json.loads(api_request.content)
-    except Exception as e:
-        api = "Error..."
 
-    return render(request, 'home.html', {'api': api})
+        api_request = requests.get("https://cloud.iexapis.com/stable/stock/" + ticker + "/quote?token=pk_1d29f40ff033462a8b4783bfae48aa78")
+
+        try:
+            api = json.loads(api_request.content)
+        except Exception as e:
+            api = "Error..."
+
+        return render(request, 'home.html', {'api': api})
+
+    else:
+        return render(request, 'home.html', {'ticker': "Enter a Ticker Symbol Above..."})
+
+    
     
 def about(request):
     return render(request, 'about.html', {})
+
+def add_stock(request):
+    return render(request, 'add_stock.html', {})
